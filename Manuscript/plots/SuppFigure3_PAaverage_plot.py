32e07f298f22afc4ee5d4ce39697757c443054c1
retinotopyproject <69472896+retinotopyproject@users.noreply.github.com>
2023-07-03 21:45:27 +1000

    major refactoring of SuppFigure3_PAaverage_plot.py - added comments and docstrings - refactored/restructured most of code - added functionality where the hemisphere (Left/Right) can be specified in a var and plots for either hemisphere can be generated

--- Manuscript/plots/SuppFigure3_PAaverage_plot.py
+++ Manuscript/plots/SuppFigure3_PAaverage_plot.py
@@ -1,192 +1,162 @@
+import os
 import os.path as osp
 import sys
 import torch_geometric.transforms as T
 import numpy as np
 import scipy.io
-
-# For loading new curvature data
 import torch
 import nibabel as nib
-
-sys.path.append('../..')
-
-from Retinotopy.functions.def_ROIs_WangParcelsPlusFovea import roi
 from nilearn import plotting
-from Retinotopy.dataset.HCP_stdprocessing_3sets_ROI import Retinotopy
 from torch_geometric.data import DataLoader
 
-
-# subject_index = 7
-
-# hcp_id = ['617748', '191336', '572045', '725751', '198653',
-#           '601127', '644246', '191841', '680957', '157336']
-
-path_curv = './../../Retinotopy/data/raw/converted'
-# curv_old = scipy.io.loadmat(osp.join(path_curv, 'cifti_curv_all.mat'))[
-#     'cifti_curv']
-
-# For new curvature data:
-
-# Defining number of nodes
-number_cortical_nodes = int(64984)
-number_hemi_nodes = int(number_cortical_nodes / 2)
-
-# Loading new curvature data for the first subject in training set (background):
-first_subject = 100610
-new_data = nib.load(osp.join(path_curv, '../..', f'raw/converted/fs-curvature/{first_subject}/', \
-    str(first_subject) + '.L.curvature.32k_fs_LR.shape.gii'))
-new_data = torch.tensor(np.reshape(new_data.agg_data().reshape((number_hemi_nodes)), 
-    (-1, 1)), dtype=torch.float)
-# # Filter out NaNs if required
-# new_data = new_data.masked_fill_(torch.tensor(np.reshape(torch.any(new_data.isnan(), dim=1).reshape((number_hemi_nodes)),
-#     (-1, 1))), 0)
-# # new_data = new_data[~torch.any(new_data.isnan())]
-# Left hemisphere:
-background = np.array(np.reshape(new_data.detach().numpy()[0:number_hemi_nodes], (-1)))
+from Retinotopy.functions.def_ROIs_WangParcelsPlusFovea import roi
+from Retinotopy.dataset.HCP_stdprocessing_3sets_ROI import Retinotopy
 
 
-# Left hemisphere:
-# background = np.zeros((number_hemi_nodes, 1))
+"""
+This code was taken from the deepRetinotopy repository, from the file 
+'SuppFigure3_PAaverage_plot.py' in the Manuscript dir
+(https://github.com/Puckett-Lab/deepRetinotopy/)
 
-# # Right hemisphere
-# background = np.reshape(
-#     curv['x' + hcp_id[subject_index] + '_curvature'][0][0][32492:], (-1))
+The code generates a mean Polar Angle map of observed (ground truth) values
+for all HCP participants. This file has been modified to generate these maps 
+for HCP data pre-processed using a standard processing pipeline (different to 
+the HCP-specific processing pipeline).
+A map of the first participants' curvature data will be used as a background
+to the mean PA map on the plotted surface.
+Mean PA maps are generated per hemisphere (either Left or Right hemisphere).
+"""
 
-threshold = 1  # threshold for the curvature map
+#### Params used for model predictions ####
+# Which hemisphere will predictions be generated for? ('Left'/'Right')
+hemisphere = 'Left'
 
-# Background settings
+# Create the file name components for the chosen prediction params
+HEMI_FILENAME = hemisphere[0]
+
+
+# The number of participants (total) in all model sets
+N_EXAMPLES = 181
+# Total number of cortical nodes in the mesh
+NUMBER_CORTICAL_NODES = int(64984)
+# Number of nodes within each hemisphere
+NUMBER_HEMI_NODES = int(NUMBER_CORTICAL_NODES / 2)
+
+
+# Configure filepaths
+sys.path.append('../..')
+# For loading data from the Training set
+path = osp.join(osp.dirname(osp.realpath(__file__)), '../..', 'Retinotopy/data')
+# For loading participants' curvature data
+path_curv = osp.join(path, 'raw/converted')
+
+# Load participant IDs, in the order they were selected for train, dev, test sets
+with open(osp.join(path, '..', 'participant_IDs_in_order.txt')) as fp:
+    subj = fp.read().split("\n")
+subj = subj[0:len(subj) - 1]
+'''
+Get the ID of the first participant in the HCP Train set. The curvature data 
+for this participant is used as a background on the plotted surface.
+'''
+first_subj = subj[0]
+
+
+#### Loading curvature data for the first subject in training set ####
+curv_data = nib.load(osp.join(path_curv, '../..', 
+    f'raw/converted/fs-curvature/{first_subj}/', str(first_subj) + 
+    f'.{HEMI_FILENAME}.curvature.32k_fs_LR.shape.gii'))
+curv_data = torch.tensor(np.reshape(
+                curv_data.agg_data().reshape((NUMBER_HEMI_NODES)), (-1, 1)), 
+                dtype=torch.float)
+
+# Set the background 
+background = np.array(np.reshape(
+                        curv_data.detach().numpy()[0:NUMBER_HEMI_NODES], (-1)))
+
+threshold = 1  # Threshold for the curvature map
+
+# Remove NaNs from curvature map
 nocurv = np.isnan(background)
 background[nocurv == 1] = 0
-background[background > 0] = 0.3
-background[background < 0] = 0.5
-# background[background < 0] = 0
-# background[background > 0] = 1
+# Background settings (discretize curvature values to give a 2 colour map)
+# background[background > 0] = 0.3
+# background[background < 0] = 0.5
+background[background < 0] = 0
+background[background > 0] = 1
 
-# Loading training data
-path = osp.join(osp.dirname(osp.realpath(__file__)), '../..', 'Retinotopy/data')
+
+#### Loading Polar Angle data for all subjects in Train set ####
+
+# A pre-transform to be applied to the data
 pre_transform = T.Compose([T.FaceToEdge()])
-# train_dataset_right = Retinotopy(path, 'Train', transform=T.Cartesian(),
-#                                  pre_transform=pre_transform, n_examples=181,
-#                                  prediction='polarAngle', myelination=True,
-#                                  hemisphere='Right')
-# train_loader_right = DataLoader(train_dataset_right, batch_size=1,
-#                                 shuffle=True)
 
-# For left hemisphere (no myelination):
-train_dataset_left = Retinotopy(path, 'Train', transform=T.Cartesian(),
-                                 pre_transform=pre_transform, n_examples=181,
-                                 prediction='polarAngle', myelination=False,
-                                 hemisphere='Left')
-train_loader_left = DataLoader(train_dataset_left, batch_size=1,
-                                shuffle=True)
+# Load the Training set
+train_dataset = Retinotopy(path, 'Train', transform=T.Cartesian(),
+                                 pre_transform=pre_transform, 
+                                 n_examples=N_EXAMPLES, prediction='polarAngle', 
+                                 myelination=False, hemisphere=hemisphere)
+train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
 
-# ROI settings
+# Selecting all visual areas (Wang2015) plus V1-3 fovea
 label_primary_visual_areas = ['ROI']
 final_mask_L, final_mask_R, index_L_mask, index_R_mask = roi(
     label_primary_visual_areas)
-PolarAngle = np.zeros((32492, 1))
 
-# Mean polar angle map
-# PA = []
-# for data in train_loader_right:
-#     PA.append(np.array(data.y))
-# PA = np.mean(PA, 0)
 
-# For left hemisphere:
+#### Get the mean PA map ####
+
+# Create an array of zeroes for PA map values
+PolarAngle = np.zeros((NUMBER_HEMI_NODES, 1))
+
+# Load all PA maps for each Train set participant
 PA = []
-for data in train_loader_left:
+for data in train_loader:
     PA.append(np.array(data.y))
+# Calculate a mean map
 PA = np.mean(PA, 0)
 
 # Create an output folder if it doesn't already exist
 directory = './output'
 if not osp.exists(directory):
-    osp.makedirs(directory)
+    os.makedirs(directory)
 
 # Saving the average map
-np.savez('./output/AveragePolarAngleMap_RH.npz', list=PA)
+np.savez(f'./output/AveragePolarAngleMap_{HEMI_FILENAME}H.npz', list=PA)
 
-# Settings for plot
-# PolarAngle[final_mask_R == 1] = np.reshape(PA, (-1, 1))
-# Left hemisphere:
-PolarAngle[final_mask_L == 1] = np.reshape(PA, (-1, 1))
-PolarAngle = np.array(PolarAngle)
-minus = PolarAngle > 180
-sum = PolarAngle < 180
-PolarAngle[minus] = PolarAngle[minus] - 180
-PolarAngle[sum] = PolarAngle[sum] + 180
 
-# Masking
-# PolarAngle[final_mask_R == 1] += threshold
-# PolarAngle[final_mask_R != 1] = 0
-# Left hemisphere:
-PolarAngle[final_mask_L == 1] += threshold
-PolarAngle[final_mask_L != 1] = 0
+#### Settings for plot ####
+if hemisphere == 'Left':
+    PolarAngle[final_mask_L == 1] = np.reshape(PA, (-1, 1))
+    # Translating Left hemisphere polar angle values
+    PolarAngle = np.array(PolarAngle)
+    minus = PolarAngle > 180
+    sum = PolarAngle < 180
+    PolarAngle[minus] = PolarAngle[minus] - 180
+    PolarAngle[sum] = PolarAngle[sum] + 180
+    # Masking
+    PolarAngle[final_mask_L == 1] += threshold
+    PolarAngle[final_mask_L != 1] = 0
+else:
+    # hemisphere == 'Right'
+    PolarAngle[final_mask_R == 1] = np.reshape(PA, (-1, 1))
+    # Masking
+    PolarAngle[final_mask_R == 1] += threshold
+    PolarAngle[final_mask_R != 1] = 0
 
+
+#### Create the surf plot ####
 view = plotting.view_surf(
     surf_mesh=osp.join(osp.dirname(osp.realpath(__file__)), '../..',
-                       'Retinotopy/data/raw/surfaces'
-                       '/S1200_7T_Retinotopy181.L.sphere.32k_fs_LR.surf.gii'),
-    surf_map=np.reshape(PolarAngle[0:32492], (-1)), bg_map=background,
-    cmap='gist_rainbow', black_bg=False, symmetric_cmap=False,
-    threshold=threshold, vmax=361,
-    title=f'Polar angle Left hemisphere mean ground truth (training set)')
+                       'Retinotopy/data/raw/surfaces' +
+                       f'/S1200_7T_Retinotopy181.{HEMI_FILENAME}' +
+                       '.sphere.32k_fs_LR.surf.gii'),
+    surf_map=np.reshape(PolarAngle[0:NUMBER_HEMI_NODES], (-1)), 
+    bg_map=background, cmap='gist_rainbow', black_bg=False, 
+    symmetric_cmap=False, threshold=threshold, vmax=361,
+    title=f'Polar angle {hemisphere} hemisphere mean ground truth (training set)')
+
+# Show in web browser
 view.open_in_browser()
-view.save_as_html(f'D:\\Retinotopy Project\\surf_images\\PA_LH_mean_trainset\\empirical_mean_trainset_LH')
+# Save plot as a HTML file
+# view.save_as_html(f'D:\\Retinotopy Project\\surf_images\\PA_{HEMI_FILENAME}H_mean_trainset\\empirical_mean_trainset_LH')
 
-
-
-# # Left hemisphere
-# background = np.reshape(
-#     curv['x' + hcp_id[subject_index] + '_curvature'][0][0][0:32492], (-1))
-
-# threshold = 1  # threshold for the curvature map
-
-# # Background settings
-# nocurv = np.isnan(background)
-# background[nocurv == 1] = 0
-# background[background < 0] = 0
-# background[background > 0] = 1
-
-# # Loading training data
-# train_dataset_left = Retinotopy(path, 'Train', transform=T.Cartesian(),
-#                                 pre_transform=pre_transform, n_examples=181,
-#                                 prediction='polarAngle', myelination=True,
-#                                 hemisphere='Left')
-# train_loader_left = DataLoader(train_dataset_left, batch_size=1, shuffle=True)
-
-# # ROI settings
-# label_primary_visual_areas = ['ROI']
-# final_mask_L, final_mask_R, index_L_mask, index_R_mask = roi(
-#     label_primary_visual_areas)
-# PolarAngle = np.zeros((32492, 1))
-
-# # Mean polar angle map
-# PA = []
-# for data in train_loader_left:
-#     PA.append(np.array(data.y))
-# PA = np.mean(PA, 0)
-
-# # Saving the average map
-# np.savez('./output/AveragePolarAngleMap_LH.npz', list=PA)
-
-# # Settings for plot
-# PolarAngle[final_mask_L == 1] = np.reshape(PA, (-1, 1))
-# PolarAngle = np.array(PolarAngle)
-# minus = PolarAngle > 180
-# sum = PolarAngle < 180
-# PolarAngle[minus] = PolarAngle[minus] - 180
-# PolarAngle[sum] = PolarAngle[sum] + 180
-
-# # Masking
-# PolarAngle[final_mask_L == 1] += threshold
-# PolarAngle[final_mask_L != 1] = 0
-
-# view = plotting.view_surf(
-#     surf_mesh=osp.join(osp.dirname(osp.realpath(__file__)), '../..',
-#                        'Retinotopy/data/raw/surfaces'
-#                        '/S1200_7T_Retinotopy181.L.sphere.32k_fs_LR.surf.gii'),
-#     surf_map=np.reshape(PolarAngle[0:32492], (-1)), bg_map=background,
-#     cmap='gist_rainbow_r', black_bg=False, symmetric_cmap=False,
-#     threshold=threshold, vmax=361)
-# view.open_in_browser()
